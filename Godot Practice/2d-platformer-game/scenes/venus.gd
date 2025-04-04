62b3bb925bdb8f32108d210a961d000ca34c4d53
SawyerMontembeau <smontembeau@gmail.com>
2025-04-04 17:34:08 -0400

    Venus Enemy Animation/player detection

--- Godot Practice/2d-platformer-game/scenes/venus.gd
+++ Godot Practice/2d-platformer-game/scenes/venus.gd
@@ -1,15 +1,29 @@
-extends CharacterBody2D
+extends StaticBody2D
 
 var timeClosing = 1
 var is_catching = false
 
 func _physics_process(delta: float) -> void:
 	if $PlayerDetect.is_colliding() and not is_catching:
+		is_catching = true
 		$Sprite2D.play("Closing")
-	move_and_slide()
+		
 
-
-func _on_sprite_2d_animation_finished(anim_name):
-	if(anim_name == ("Closing")):
+func _on_sprite_2d_animation_finished() -> void:
+	if($Sprite2D.animation == ("Closing")):
 		$Sprite2D.play("Closed")
+		$ClosedHitbox.disabled = false
+		return
+		
+	if($Sprite2D.animation == ("Closed")):
+		$Sprite2D.play("Opening")
+		$ClosedHitbox.disabled = true
+		return
+		
+	if($Sprite2D.animation == ("Opening")):
+		$Sprite2D.play("Open")
 		is_catching = false
+		$ClosedHitbox.disabled = true
+		return
+	
+	
