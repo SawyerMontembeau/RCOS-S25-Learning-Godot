39236f9da9a3b29145aacc36d5c4fb6c96693121
SawyerMontembeau <smontembeau@gmail.com>
2025-04-04 15:57:59 -0400

    Tuesday changes

--- Godot Practice/2d-platformer-game/scenes/venus.gd
+++ Godot Practice/2d-platformer-game/scenes/venus.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 var timeClosing = 1
+var is_catching = false
 
 func _physics_process(delta: float) -> void:
-	
-	if($PlayerDetect.is_colliding()):
+	if $PlayerDetect.is_colliding() and not is_catching:
 		$Sprite2D.play("Closing")
-	
-		
-		
-	
-	
-
 	move_and_slide()
+
+
+func _on_sprite_2d_animation_finished(anim_name):
+	if(anim_name == ("Closing")):
+		$Sprite2D.play("Closed")
+		is_catching = false
